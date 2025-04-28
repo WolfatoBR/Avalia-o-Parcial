d8e5fef40452679a314607c159cf3d8544a3a791
daev <daavissz.03@gmail.com>
2025-04-28 17:56:43 -0300

    menu superior & botão Zap

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,4 +1,14 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('.menu-lateral').classList.toggle('ativo');
+    const menuItens = document.querySelector('.menu-itens');
+    menuItens.classList.toggle('ativo');
+    
+    // Alterna ícone do botão
+    const icon = this.querySelector('i');
+    if (menuItens.classList.contains('ativo')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
 });
-
